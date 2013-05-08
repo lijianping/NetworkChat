e0d1c522b7f67af560c3111ecdd38ad2e3ceb08e
lijianping <itisabig@hotmail.com>
2013-05-08 16:55:00 +0800

    Update main.h

--- networkchar/networkchat_server/networkchat_server/main.h
+++ networkchar/networkchat_server/networkchat_server/main.h
@@ -2,21 +2,19 @@
 #include <iostream>
 #include <winsock2.h>
 #pragma comment(lib, "WS2_32")
-using namespace std;
 
-//�û���Ϣ�ṹ�壨�û�����map��keyֵ���û���Ϣ��value��
 typedef struct USER_INFO
 {
 	SOCKET user_socket;
 	sockaddr_in user_addr;
 }*pUSER_INFO;
 
-//��Ϣ�ṹ��
+// the message information
 typedef struct USER_MESSAGE
 {
-	int message_mark;
-	std::string user_name;
-	std::string message_info;
+	int message_mark;            // the message indentify
+	std::string user_name;       // who send the message
+	std::string message_data;
 }*pUSER_MESSAGE;
 
 
